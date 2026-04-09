6f7aefe09fb42d3f5ef9382a6b081b21cd9f5791
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-04-09 14:05:47 +0200

    Entities

---
+++ WareHouse-App/WareHouse-App/Entities/InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿namespace WareHouse_App.Entities;
+
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public required int Quantity { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+    public int WarehouseId { get; set; }
+    public Warehouse Warehouse { get; set; } = null!;
+
+    public ICollection<StockMovement> Movements { get; } = [];
+
+
+}
