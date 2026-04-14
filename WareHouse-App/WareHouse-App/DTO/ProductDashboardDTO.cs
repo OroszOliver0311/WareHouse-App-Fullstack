e6d1493312974cd1cd508e2bf0ecaf47576b7a21
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-04-14 17:09:32 +0200

    dashboard DTO

---
+++ WareHouse-App/WareHouse-App/DTO/ProductDashboardDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WareHouse_App.DTO;
+
+public class ProductDashboardDTO
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string SKU{ get; set; }
+    public required decimal Price { get; set; }
+    public int TotalStock { get; set; }
+}
