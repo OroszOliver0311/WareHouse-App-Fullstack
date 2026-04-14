72bb9335543059f4e0be36373253c2d34c004955
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-04-14 16:59:58 +0200

    DbSeeder

--- WareHouse-App/WareHouse-App/Program.cs
+++ WareHouse-App/WareHouse-App/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<WareHouse_App.Data.WarehouseDbContext>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<WareHouse_App.Data.WarehouseDbContext>();
+    WareHouse_App.Data.DbSeeder.Seed(context);
+}
 
+
+
+app.MapControllers();
 app.Run();
