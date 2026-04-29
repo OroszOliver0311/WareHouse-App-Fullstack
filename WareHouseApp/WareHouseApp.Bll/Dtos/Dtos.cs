ef9abbd7fe24c70ca2c4573d6fb5a8b95f238f9b
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-04-29 14:35:47 +0200

    DashboardBLl, api

---
+++ WareHouseApp/WareHouseApp.Bll/Dtos/Dtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouseApp.Bll.Dtos;
+
+public record ProductDashboardDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = null!;
+    public string SKU { get; init; } = null!;
+    public int TotalQuantity { get; init; }
+}
---
+++ WareHouseApp/WareHouseApp.Bll/Interfaces/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using WareHouseApp.Bll.Dtos;
+
+namespace WareHouseApp.Bll.Interfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDashboardDto>> GetDashboardAsync();
+}
