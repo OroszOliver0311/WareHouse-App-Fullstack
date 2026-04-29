bb8cf5f7fde234719a3ca73354ce64608f08a24b
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-04-29 14:35:47 +0200

    DashboardBLl, api

---
+++ WareHouseApp/WareHouseApp.Bll/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WareHouseApp.Bll.Dtos;
+using WareHouseApp.Bll.Interfaces;
+using WareHouseApp.Dal;
+
+namespace WareHouseApp.Bll.Services;
+
+public class ProductService(AppDbContext context) : IProductService
+{
+    public async Task<IEnumerable<ProductDashboardDto>> GetDashboardAsync()
+    {
+        var products = await context.Products
+                .Select(p => new ProductDashboardDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    SKU = p.SKU,
+                    TotalQuantity = p.InventoryItems.Sum(i => i.Quantity)
+                })
+                .ToListAsync();
+        return products;
+    }
+
+}
