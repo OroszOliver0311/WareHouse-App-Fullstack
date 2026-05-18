716b2cca997ef84049975b7aab44288e0bbc3faf
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-05-18 17:29:00 +0200

    stock update

--- Frontend/src/app/services/product.ts
+++ Frontend/src/app/services/product.ts
@@ -8,18 +8,25 @@ import { ProductDashboardDto, ProductDetailDto } from '../models/product.dto';
 })
 export class ProductService {
 private readonly http = inject(HttpClient);
-private readonly apiUrl = 'https://localhost:7122/api/Products';
+private readonly productApiUrl = 'https://localhost:7122/api/Products';
+private readonly inventoryApiUrl = 'https://localhost:7122/api/Inventory';
 
 getDashboard(): Observable<ProductDashboardDto[]> {
-  return this.http.get<ProductDashboardDto[]>(`${this.apiUrl}/dashboard`);
+  return this.http.get<ProductDashboardDto[]>(`${this.productApiUrl}/dashboard`);
 }
 getProductById(id: number): Observable< ProductDetailDto> {
-  return this.http.get<ProductDetailDto>(`${this.apiUrl}/${id}`);
+  return this.http.get<ProductDetailDto>(`${this.productApiUrl}/${id}`);
 }
 updateProduct(id: number, productData: any): Observable<any> {
-  return this.http.put(`${this.apiUrl}/${id}`, productData);
+  return this.http.put(`${this.productApiUrl}/${id}`, productData);
 }
+updateStockQuantity(productId: number, warehouseId: number, quantity: number): Observable<any> {
+  const payload = {
+    productId: productId,
+    wareHouseId: warehouseId,
+    quantity: quantity
+  };
 
-
-
+  return this.http.post(`${this.inventoryApiUrl}/upsert`, payload);
+}
 }
