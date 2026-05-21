69145875888419520ba704b5a9d87c5b3f8cb10b
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-05-21 19:50:02 +0200

    angolra forditas

--- src/Backend/WareHouseApp.Api/Controllers/StockMovementController.cs
+++ src/Backend/WareHouseApp.Api/Controllers/StockMovementController.cs
@@ -6,9 +6,18 @@ namespace WareHouseApp.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+[ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
+[ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
 public class StockMovementsController(IStockMovementService stockMovementService) : ControllerBase
 {
+    /// <summary>
+    /// Queries the stock movement history for a specific product by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the product for which to query movement history.</param>
+    /// <returns>The list of stock movements for the specified product, ordered from most recent to oldest.</returns>
     [HttpGet("product/{id:int}")]
+    [ProducesResponseType<IEnumerable<StockMovementDto>>(StatusCodes.Status200OK)]
     public async Task<ActionResult<IEnumerable<StockMovementDto>>> GetProductHistory(int id)
     {
         var history = await stockMovementService.GetProductHistoryAsync(id);
