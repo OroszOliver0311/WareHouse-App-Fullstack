396fbfa141d6812e25cfa86d89406a130bd3273e
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-05-22 14:25:06 +0200

    tessts v2.0

---
+++ src/Backend/WareHouseApp.Test/ApiIntegrationTest.cs
@@ -0,0 +1,106 @@
+﻿using Bogus;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using WareHouseApp.Bll.Dtos;
+using WareHouseApp.Test;
+
+namespace WareHouseApp.Tests;
+
+public class ApiIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+
+    public ApiIntegrationTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+    }
+
+
+    [Fact]
+    public async Task GetWareHouses_ReturnsOk_WithSeedData()
+    {
+
+        var response = await _client.GetAsync("/api/warehouses");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var warehouses = await response.Content.ReadFromJsonAsync<IEnumerable<WareHouseDto>>(_factory.SerializerOptions);
+        warehouses.Should().NotBeNullOrEmpty();
+
+        warehouses.Should().Contain(w => w.Name == "Budapesti Központi Raktár");
+    }
+
+    [Fact]
+    public async Task GetV2Test_ReturnsOk_StringMessage()
+    {
+
+        var response = await _client.GetAsync("/api/warehouses/v2-test");
+
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var message = await response.Content.ReadAsStringAsync();
+        message.Should().Contain("V2");
+    }
+
+
+    [Fact]
+    public async Task CreateProduct_ReturnsCreated_WithValidBogusData()
+    {
+
+        var faker = new Faker<CreateProductDto>("en")
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.SKU, f => f.Commerce.Ean13())
+            .RuleFor(p => p.UnitPrice, f => decimal.Parse(f.Commerce.Price(1000, 50000)));
+
+        var newProductDto = faker.Generate();
+
+
+        var response = await _client.PostAsJsonAsync("/api/products", newProductDto, _factory.SerializerOptions);
+
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        response.Headers.Location.Should().NotBeNull();
+
+        var createdProduct = await response.Content.ReadFromJsonAsync<ProductDetailDto>(_factory.SerializerOptions);
+        createdProduct.Should().NotBeNull();
+        createdProduct!.Id.Should().BeGreaterThan(0); 
+        createdProduct.Name.Should().Be(newProductDto.Name);
+    }
+
+
+    [Fact]
+    public async Task GetProductDetails_ReturnsNotFound_WhenIdDoesNotExist()
+    {
+    
+        var response = await _client.GetAsync("/api/products/99999");
+
+  
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<Microsoft.AspNetCore.Mvc.ProblemDetails>(_factory.SerializerOptions);
+        problemDetails.Should().NotBeNull();
+        problemDetails!.Status.Should().Be(404);
+    }
+
+
+    [Fact]
+    public async Task DeleteProduct_ReturnsNoContent_WhenProductExists()
+    {
+        var createDto = new CreateProductDto { Name = "Product To Delete", SKU = "DEL-123", UnitPrice = 1000 };
+        var createResponse = await _client.PostAsJsonAsync("/api/products", createDto, _factory.SerializerOptions);
+        var createdProduct = await createResponse.Content.ReadFromJsonAsync<ProductDetailDto>(_factory.SerializerOptions);
+
+        var idToDelete = createdProduct!.Id;
+
+        var deleteResponse = await _client.DeleteAsync($"/api/products/{idToDelete}");
+
+        deleteResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+
+        var getResponse = await _client.GetAsync($"/api/products/{idToDelete}");
+        getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
---
+++ src/Backend/WareHouseApp.Test/ControllerUnitTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WareHouseApp.Api.Controllers;
+using WareHouseApp.Bll.Dtos;
+using WareHouseApp.Bll.Exceptions;
+using WareHouseApp.Bll.Interfaces;
+
+namespace WareHouseApp.Tests;
+
+public class ControllerUnitTests
+{
+    [Fact]
+    public async Task GetAllWareHouses_ReturnsOk_WithListOfWareHouses()
+    {
+        var mockService = new Mock<IWareHouseService>();
+        var fakeData = new List<WareHouseDto>
+        {
+            new WareHouseDto { Id = 1, Name = "Test Warehouse", Location = "Pécs" }
+        };
+        mockService.Setup(s => s.GetAllWareHousesAsync()).ReturnsAsync(fakeData);
+
+        var controller = new WareHousesController(mockService.Object);
+
+        var result = await controller.GetAllWareHouses();
+
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        var returnValue = okResult.Value.Should().BeAssignableTo<IEnumerable<WareHouseDto>>().Subject;
+        returnValue.Should().HaveCount(1);
+        returnValue.First().Name.Should().Be("Test Warehouse");
+    }
+
+    [Fact]
+    public async Task GetWareHouseById_ReturnsOk_WhenWareHouseExists()
+    {
+        var mockService = new Mock<IWareHouseService>();
+        var fakeDto = new WareHouseDto { Id = 5, Name = "Test Warehouse", Location = "Debrecen" };
+        mockService.Setup(s => s.GetWareHouseByIdAsync(5)).ReturnsAsync(fakeDto);
+
+        var controller = new WareHousesController(mockService.Object);
+
+        var result = await controller.GetWareHouseById(5);
+
+
+        var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+        var returnedDto = okResult.Value.Should().BeOfType<WareHouseDto>().Subject;
+        returnedDto.Id.Should().Be(5);
+    }
+
+    [Fact]
+    public async Task GetWareHouseById_ReturnsNotFound_WhenEntityDoesNotExist()
+    {
+
+        var mockService = new Mock<IWareHouseService>();
+        mockService.Setup(s => s.GetWareHouseByIdAsync(99))
+                   .ThrowsAsync(new EntityNotFoundException("WareHouse", 99));
+
+        var controller = new WareHousesController(mockService.Object);
+
+        var result = await controller.GetWareHouseById(99);
+
+        result.Result.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Fact]
+    public async Task CreateProduct_ReturnsCreatedAtAction_WithCreatedProduct()
+    {
+
+        var mockService = new Mock<IProductService>();
+        var createDto = new CreateProductDto { Name = "Mouse", SKU = "123", UnitPrice = 1000 };
+        var createdDto = new ProductDetailDto { Id = 1, Name = "Mouse", SKU = "123", UnitPrice = 1000 };
+
+        mockService.Setup(s => s.CreateProductAsync(createDto)).ReturnsAsync(createdDto);
+        var controller = new ProductsController(mockService.Object);
+
+
+        var result = await controller.CreateProduct(createDto);
+
+
+        var createdResult = result.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+        createdResult.ActionName.Should().Be(nameof(ProductsController.GetDetails));
+        createdResult.RouteValues!["id"].Should().Be(1); 
+        createdResult.Value.Should().BeEquivalentTo(createdDto);
+    }
+
+
+    [Fact]
+    public async Task DeleteProduct_ReturnsNoContent_WhenSuccessful()
+    {
+
+        var mockService = new Mock<IProductService>();
+        mockService.Setup(s => s.DeleteProductAsync(1)).Returns(Task.CompletedTask);
+        var controller = new ProductsController(mockService.Object);
+
+
+        var result = await controller.DeleteProduct(1);
+
+
+        result.Should().BeOfType<NoContentResult>();
+
+        mockService.Verify(s => s.DeleteProductAsync(1), Times.Once);
+    }
+}
